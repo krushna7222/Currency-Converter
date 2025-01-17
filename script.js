81770273f429545a1c18b765e765f6ad70086885
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-01-17 21:55:33 +0530

    HTML Format File For Currency Converter

--- script.js
+++ script.js
@@ -1,6 +1,66 @@
-for (let code in countryList) {
-  console.log(code);
+// const FIRST_URL =
+//   "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+const FIRST_URL =
+  "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@/{apiVersion}/{endpoint}";
+
+const dropdowns = document.querySelectorAll("select");
+const btn = document.querySelector("button");
+const fromCurr = document.querySelector("select");
+const toCurr = document.querySelector("select");
+const msg = document.querySelector("#result");
+
+for (let select of dropdowns) {
+  for (currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
 }
-for (let code in countryList) {
-  console.log(code);
-}
+
+const updateExchangeRate = async () => {
+  let amount = document.querySelector(".input input");
+  console.log(amount.value);
+  let amtVal = amount.value;
+  console.log(amtVal);
+
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+  const URL = `${FIRST_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+  let response = await fetch(URL);
+  let data = await response.json();
+  console.log(data);
+
+  let rate = data[toCurr.value.toLowerCase()];
+
+  let finalAmount = amtVal * rate;
+  msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+};
+
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+window.addEventListener("load", () => {
+  updateExchangeRate();
+});
